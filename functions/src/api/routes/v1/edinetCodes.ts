7d5bc9f0aadd50b88d0251415fa3d78c25aa93f9
IkumaHayashi <ikuma.hayashi.tech@gmail.com>
2022-08-14 22:04:21 +0900

    fix

--- functions/src/api/routes/v1/edinetCodes.ts
+++ functions/src/api/routes/v1/edinetCodes.ts
@@ -1,27 +1,27 @@
-import { EdinetCode, RawEdinetCode } from "common";
+import {EdinetCode, RawEdinetCode} from "common";
 import EdinetcodeDlInfo from "./EdinetcodeDlInfo.json" assert { type: "json" };
 import express from "express";
 // eslint-disable-next-line new-cap
 const router = express.Router();
 router.get("/", (req, res) => {
   const edinetCodes = (EdinetcodeDlInfo as Array<RawEdinetCode>).map(
-    (rawEdinetCode) => EdinetCode.fromJson(rawEdinetCode)
+      (rawEdinetCode) => EdinetCode.fromJson(rawEdinetCode)
   );
   res
-    .status(200)
-    .json(edinetCodes.filter((edinetCode) => edinetCode.listed === "上場"));
+      .status(200)
+      .json(edinetCodes.filter((edinetCode) => edinetCode.listed === "上場"));
 });
 router.get("/:fundCode", (req, res) => {
   const edinetCodes = (EdinetcodeDlInfo as Array<RawEdinetCode>).map(
-    (rawEdinetCode) => EdinetCode.fromJson(rawEdinetCode)
+      (rawEdinetCode) => EdinetCode.fromJson(rawEdinetCode)
   );
   const fundCode = String(
-    req.params.fundCode.length === 4
-      ? req.params.fundCode + "0"
-      : req.params.fundCode
+    req.params.fundCode.length === 4 ?
+      req.params.fundCode + "0" :
+      req.params.fundCode
   );
   const edinetCode = edinetCodes.filter(
-    (edinetCode) => edinetCode.fund_code === fundCode
+      (edinetCode) => edinetCode.fund_code === fundCode
   );
   res.status(200).json(edinetCode);
 });
