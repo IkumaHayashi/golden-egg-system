f216c2bf7b2dd9ab4df7840d3081f3fb4a46b27e
IkumaHayashi <ikuma.hayashi.tech@gmail.com>
2022-08-14 22:21:08 +0900

    fix

--- functions/src/api/api.ts
+++ functions/src/api/api.ts
@@ -3,8 +3,8 @@ import * as functions from "firebase-functions";
 
 const app = express();
 
-import {edinetCodes} from "./routes/v1/edinetCodes.js";
+import { edinetCodes } from "./routes/v1/companies.js";
 app.use("/v1/edinetCodes", edinetCodes);
 const api = functions.region("asia-northeast1").https.onRequest(app);
 
-export {api};
+export { api };
