267859ff2375042817c1affbdf380302c6dd524d
Ikuma Hayashi <ikuma.hayashi.tech@gmail.com>
2022-08-15 16:28:16 +0900

    株価をAPIの戻り値含めるように変更 (#5)

---
+++ common/src/api/companies.ts
@@ -0,0 +1,7 @@
+import { EdinetCode } from "../firestore/models/edinetCode";
+
+interface ICompanyResponse extends EdinetCode {
+  price: number | undefined;
+  currency: string | undefined;
+}
+export { ICompanyResponse };
--- functions/src/api/routes/v1/companies.ts
+++ functions/src/api/routes/v1/companies.ts
@@ -1,28 +1,38 @@
-import {EdinetCode, RawEdinetCode} from "common";
+import { EdinetCode, RawEdinetCode, ICompanyResponse } from "common";
 import EdinetcodeDlInfo from "./EdinetcodeDlInfo.json" assert { type: "json" };
 import express from "express";
+import yahooFinance from "yahoo-finance2";
+
 // eslint-disable-next-line new-cap
 const router = express.Router();
 router.get("/", (req, res) => {
   const companies = (EdinetcodeDlInfo as Array<RawEdinetCode>).map(
-      (rawEdinetCode) => EdinetCode.fromJson(rawEdinetCode)
+    (rawEdinetCode) => EdinetCode.fromJson(rawEdinetCode)
   );
   res
-      .status(200)
-      .json(companies.filter((edinetCode) => edinetCode.listed === "上場"));
+    .status(200)
+    .json(companies.filter((edinetCode) => edinetCode.listed === "上場"));
 });
-router.get("/:fundCode", (req, res) => {
+router.get("/:fundCode", async (req, res) => {
   const companies = (EdinetcodeDlInfo as Array<RawEdinetCode>).map(
-      (rawEdinetCode) => EdinetCode.fromJson(rawEdinetCode)
+    (rawEdinetCode) => EdinetCode.fromJson(rawEdinetCode)
   );
   const fundCode = String(
-    req.params.fundCode.length === 4 ?
-      req.params.fundCode + "0" :
-      req.params.fundCode
+    req.params.fundCode.length === 4
+      ? req.params.fundCode + "0"
+      : req.params.fundCode
   );
+  const fundCode4 = fundCode.substring(0, 4);
+  const results = await yahooFinance.quote(fundCode4 + ".T");
   const edinetCode = companies.filter(
-      (edinetCode) => edinetCode.fund_code === fundCode
-  );
-  res.status(200).json(edinetCode);
+    (edinetCode) => edinetCode.fund_code === fundCode
+  )[0];
+  const response: ICompanyResponse = {
+    ...edinetCode,
+    price: results.regularMarketPrice,
+    currency: results.currency,
+  };
+  res.status(200).json(response);
 });
+
 export const companies = router;
