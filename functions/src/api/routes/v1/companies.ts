6cca2a961a8ae235a4aaf500a76647cb7743a62f
Ikuma Hayashi <ikuma.hayashi.tech@gmail.com>
2022-08-18 13:37:54 +0900

    APIにsearchを追加、株価・配当金・配当利回りを返すようにした (#6)
    * 株価をAPIの戻り値含めるように変更
    
    * searchメソッド追加
    
    * 配当金を返すように変更

--- functions/src/api/routes/v1/companies.ts
+++ functions/src/api/routes/v1/companies.ts
@@ -13,6 +13,39 @@ router.get("/", (req, res) => {
     .status(200)
     .json(companies.filter((edinetCode) => edinetCode.listed === "上場"));
 });
+router.post("/search", async (req, res) => {
+  const fundCodes = (req.body.codes as string).split(",") ?? [];
+  const companies = (EdinetcodeDlInfo as Array<RawEdinetCode>)
+    .map((rawEdinetCode) => EdinetCode.fromJson(rawEdinetCode))
+    .filter((edinetCode) =>
+      fundCodes.some((fundCode) => fundCode + "0" === edinetCode.fund_code)
+    );
+  const quotes = await yahooFinance.quote(
+    fundCodes.map((fundCode) => fundCode + ".T")
+  );
+
+  const responses: Array<ICompanyResponse> = companies.map((company) => {
+    const quote = quotes.find(
+      (rawQuote) => rawQuote.symbol.substring(0, 4) + "0" === company.fund_code
+    );
+    const dividendYield =
+      quote?.regularMarketPrice && quote?.regularMarketPrice
+        ? (quote?.trailingAnnualDividendRate! / quote?.regularMarketPrice!) *
+          100
+        : undefined;
+    return {
+      ...company,
+      price: quote?.regularMarketPrice,
+      currency: quote?.currency,
+      dividend: quote?.trailingAnnualDividendRate,
+      dividend_yield:
+        dividendYield !== undefined
+          ? parseFloat(dividendYield?.toFixed(2))
+          : undefined,
+    };
+  });
+  res.status(201).json(responses);
+});
 router.get("/:fundCode", async (req, res) => {
   const companies = (EdinetcodeDlInfo as Array<RawEdinetCode>).map(
     (rawEdinetCode) => EdinetCode.fromJson(rawEdinetCode)
@@ -23,14 +56,23 @@ router.get("/:fundCode", async (req, res) => {
       : req.params.fundCode
   );
   const fundCode4 = fundCode.substring(0, 4);
-  const results = await yahooFinance.quote(fundCode4 + ".T");
+  const quote = await yahooFinance.quote(fundCode4 + ".T");
   const edinetCode = companies.filter(
     (edinetCode) => edinetCode.fund_code === fundCode
   )[0];
+  const dividendYield =
+    quote?.regularMarketPrice && quote?.regularMarketPrice
+      ? (quote?.trailingAnnualDividendRate! / quote?.regularMarketPrice!) * 100
+      : undefined;
   const response: ICompanyResponse = {
     ...edinetCode,
-    price: results.regularMarketPrice,
-    currency: results.currency,
+    price: quote.regularMarketPrice,
+    currency: quote.currency,
+    dividend: quote.trailingAnnualDividendRate,
+    dividend_yield:
+      dividendYield !== undefined
+        ? parseFloat(dividendYield?.toFixed(2))
+        : undefined,
   };
   res.status(200).json(response);
 });
